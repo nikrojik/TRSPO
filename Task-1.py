9e9e9b00215320ff08420d214587fdc84b900955
nikrojik <104141802+nikrojik@users.noreply.github.com>
2022-12-07 16:52:47 +0200

    Delete Task-1.py

--- Task-1.py
+++
@@ -1,9 +0,0 @@
-import threading
-
-def sum(a, b):
-    print(a + b)
-
-thr1 = threading.Thread(target=sum, args = (1, 2))
-thr2 = threading.Thread(target=sum, args = (3, 4))
-thr1.start()
-thr2.start()
